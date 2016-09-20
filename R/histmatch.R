72791f6a47b38d390557a68a395e569e2a2eba18
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-20 09:32:18 +0200

    Revert "shift target for symmetric capture of border"
    This reverts commit 71dcc96428e014a00bbbfb8efb9592fb5d7ccf02.

--- R/histmatch.R
+++ R/histmatch.R
@@ -16,7 +16,7 @@ histmatch <- function(source, target, w = NULL) {
     x_target <- cumsum(w[y_target_order])
   }
 
-  x_target <- rescale(x_target, 0.5, length(source) + 0.5)
+  x_target <- rescale(x_target, 1, length(source))
   y_target <- target[y_target_order]
 
   #xy_source_new <- approx(x_target, y_target, x_source, ties = "ordered")
