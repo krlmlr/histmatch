b1b5f7b232edb3dbe2c87383c4a0648ef6bfc852
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-20 09:52:29 +0200

    extract function

--- R/histmatch.R
+++ R/histmatch.R
@@ -16,9 +16,13 @@ histmatch <- function(source, target, w = NULL) {
     x_target <- cumsum(w[y_target_order])
   }
 
-  x_target <- rescale(x_target, 1, length(source))
+  x_target <- rescale(x_target, 1, length(x_source))
   y_target <- target[y_target_order]
 
+  interpolate(x_target, y_target, x_source)
+}
+
+interpolate <- function(x_target, y_target, x_source) {
   #xy_source_new <- approx(x_target, y_target, x_source, ties = "ordered")
   #xy_source_new$y
   .Call(stats:::C_Approx, x_target, y_target, as.double(x_source),
